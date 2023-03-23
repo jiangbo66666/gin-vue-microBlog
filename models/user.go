5a54dd68df4da805a4ba4eeed35fbd6b8e705d6f
jiangbo <jiangbo1996@outlook.com>
2023-03-23 20:17:52 +0800

    【新增dto文件夹，存放需要返回给前端的结构体和从前端获取的结构体】

--- models/user.go
+++ models/user.go
@@ -1,23 +1,14 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 // 负责映射到前端的数据结构，需要与数据库数据分离
 // 属性名保持与数据库表创建的结构体相同
-type AccountInfoJson struct {
-	AccountName string `json:"accountName"`
-	PhoneNumber string `json:"phoneNumber"`
-	Password    string
-	RecentLogin time.Time `json:"recentLogin"`
-	Status      int       `json:"status"`
-}
 
-func AccountInfoById(id int) (AccountInfoJson, error) {
-	curUser := AccountInfoJson{}
+func AccountInfoById(id int) (AccountInfo, error) {
+	curUser := AccountInfo{}
 	// sql语句取值且放入curUser结构体
 	err := DB.Where("id = ?", id).Where("status = ?", 0).First(&AccountInfo{}).Error
 	if err != nil {
@@ -31,8 +22,8 @@ func AccountInfoById(id int) (AccountInfoJson, error) {
 	return curUser, err
 }
 
-func AccountInfoByName(name string) (AccountInfoJson, error) {
-	curUser := AccountInfoJson{}
+func AccountInfoByName(name string) (AccountInfo, error) {
+	curUser := AccountInfo{}
 	// sql语句取值且放入curUser结构体 ,并且筛选掉停用的账号
 	err := DB.Debug().Model(&AccountInfo{}).Where("account_name = ?", name).Where("status = ?", 0).Scan(&curUser).Error
 	// scan将数据库查询出来的数据扫描到与前端交互的结构体中
@@ -42,8 +33,8 @@ func AccountInfoByName(name string) (AccountInfoJson, error) {
 	return curUser, err
 }
 
-func AccountInfoByPhone(phoneNumber string) (AccountInfoJson, error) {
-	curUser := AccountInfoJson{}
+func AccountInfoByPhone(phoneNumber string) (AccountInfo, error) {
+	curUser := AccountInfo{}
 	// sql语句取值且放入curUser结构体,并且筛选掉停用的账号
 	err := DB.Model(&AccountInfo{}).Where("phone_number = ?", phoneNumber).Where("status = ?", 0).Scan(&curUser).Error
 	// scan将数据库查询出来的数据扫描到与前端交互的结构体中
